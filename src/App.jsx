46faa43b9be46c4a6fa206ae1513eb24ea7bafa8
Yeonu Jung <34713743-yeonu959@users.noreply.replit.com>
2024-06-16 05:53:29 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <h1 style={{ fontFamily: '"Comic Sans MS", fantasy', color: '#4b9be1' }}>
+        {"Blue_Rose's website"}
+      </h1>
+      <h2
+        style={{
+          fontFamily: 'Arial, Helvetica, cursive',
+          color: 'rgb(243, 103, 123)',
+          marginBottom: '7rem'
+        }}
+      >
+        click the buttons below
+      </h2>
+      <button
+        style={{
+          fontFamily: '"Comic Sans MS", fantasy',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'blue',
+          color: 'white'
+        }}
+        onClick={() => alert('Hello there')}
+      >
+        Welcome
+      </button>
+      <button
+        style={{
+          fontFamily: '"Comic Sans MS", fantasy',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'orange',
+          color: 'white',
+          marginTop: '2rem'
+        }}
+        onClick={() => {
+          const name = prompt("What's your name?");
+          if (name) {
+            alert('Nice to meet you, ' + name + '!');
+          } else {
+            alert('Nice to meet you, stranger');
+          }
+        }}
+      >
+        {"What's your name?"}
+      </button>
+    </div>
+  );
+}
+
+export default HomePage;
